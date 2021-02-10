6cc38317a7a685a5b0d402fb1f9ce20d5803b309
Kamaile Joe <kamailecarrell@google.com>
2021-02-10 05:29:32 +0000

    Model classes entered

--- model.py
+++ model.py
@@ -20,6 +20,7 @@ class Client(db.Model):
     email = db.Column(db.String, unique=True)
     location = db.Column(db.String)
     added_details = db.Column(db.String)
+    paid = db.Column(db.boolean)
 
     def __repr__(self):
         return f'<User user_id={self.user_id} phone_num={self.phone_num} email={self.email} event_type{self.event_type} location{}>'
@@ -36,8 +37,10 @@ class Staffer(db.Model):
     dept = db.Column(db.String)
     phone_num = db.Column(db.Integer)
     email = db.Column(db.String, unique=True)
-    pay = db.Column(db.Float)
+    pay_grade = db.Column(db.Float)
     work_status = db.Column(db.String)
+    emp_attributes = db.Column(db.String)
+    emp_exceptions = db.Column(db.String)
 
     def __repr__(self):
         return f'<Staffer staff_id={self.staff_id} dept={self.dept} fname={self.fname} lname={self.lname}>'
@@ -52,16 +55,39 @@ class Party_Package(db.Model):
     title = db.Column(db.String)
     overview = db.Column(db.Text)
     cost = db.Column(db.Float)
+    total_cost = db.Column(db.Float)
     party_date = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('clients.user_id')
-    staff_id = db.Column(db.Integer, db.ForeignKey('staff.user_id')
+    staff_id = db.Column(db.Integer, db.ForeignKey('staff.staff_id')
     
     user = db.relationship('Client', backref='partypackages')
     staff = db.relationship('Staffer', backref='partypackages')
 
-
     def __repr__(self):
-        return f'<Party_Package purchase_id={self.purchase_id} title={self.title}>'
+        return f'<Party_Package purchase_id={self.purchase_id} title={self.title} total_cost{self.total_cost}>'
+
+
+class Party_Purchase_List(db.Model):
+    """Items needed for party packages."""
+
+    __tablename__ = 'pplists'
+
+    pp_list_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    dept = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String)
+    qty = db.Column(db.Integer)
+    available = db.Column(db.String)
+    qty_needed = db.Column(db.Integer)
+    party_cost = db.Column(db.Float)
+    purchase_id = db.Column(db.Integer, db.ForeignKey('partypackages.purchase_id')
+    inventory_id = db.Column(db.Integer, db.ForeignKey('inventorys.inventory_id')
+
+    purchase = db.relationship('Party_Package', backref='pplists')
+    inventory = db.relationship('Inventory', backref='pplists')
+    
+    def __repr__(self):
+        return f'<Party_Package_List pp_list_id={self.pp_list_id} name={self.name} dept={self.dept} qty={self.qty} party_cost{self.party_cost}>'
 
 
 class Inventory(db.Model):
@@ -79,11 +105,8 @@ class Inventory(db.Model):
     item_dept = db.Column(db.String)
     location_warehouse = db.Column(db.String)
     
-    
-
-
     def __repr__(self):
-        return f'<Rating rating_id={self.rating_id} score={self.score}>'
+        return f'<Inventory inventory_id={self.inventory_id} name={self.inventory} item_dept={self.item_dept} qty={self.qty}>'
 
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):

fe0a1983853836901654e7de76ef9adfe6e9924c
Kamaile Joe <kamailecarrell@google.com>
2021-02-12 03:36:17 +0000

    Model Py completed with updated db model 2/11/21

--- model.py
+++ model.py
@@ -10,28 +10,104 @@ db = SQLAlchemy()
 
 
 class Client(db.Model):
-    """A user. Class will inculde Clients Info and Event Needs."""
+    """A user. Class will include Clients Info and Event Needs."""
 
     __tablename__ = 'clients'
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(50))
-    goh_name = db.Column(db.String(50))
     phone_num = db.Column(db.Integer)
-    event_type = db.Column(db.String)
     email = db.Column(db.String, unique=True)
-    location = db.Column(db.String)
+    
+    def __repr__(self):
+        return f'<User user_id={self.user_id} name={self.name}>'
+
+class Event(db.Model):
+    """Unique Event selection set by Client."""
+
+    __tablename__ = 'events'
+
+    event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    goh_name = db.Column(db.String(50))
+    date_of_event = db.Column(db.DateTime)
     added_details = db.Column(db.String)
-    paid = db.Column(db.String)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
+    purchase_id = db.Column(db.Integer, db.ForeignKey('partypackages.purchase_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('clients.user_id'))
+    
+    location = db.relationship('Location', backref='events')
+    user = db.relationship('Client', backref='events')
+    partypackage = db.relationship('Party_Package', backref='events')
 
     def __repr__(self):
-        return f'<User user_id={self.user_id} phone_num={self.phone_num} email={self.email} event_type{self.event_type} location{self.location}>'
+        return f'<Event event_id={self.event_id} goh_name={self.goh_name} 
+                                   date_of_event{self.date_of_event} party_package{self.party_package}>'
 
+class Party_Package(db.Model):
+    """Party packages."""
+
+    __tablename__ = 'partypackages'
+
+    purchase_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    cost = db.Column(db.Float)
+    qty_of_guest = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Party_Package purchase_id={self.purchase_id} title={self.title}>'
+
+class Location(db.Model):
+    """Location of Party"""
+
+    __tablename__= 'locations'
+
+    location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    city = db.Column(db.String)
+    address = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Location location_id={self.location_id} city={self.city}>'
+
+
+class Party_Purchase_Items(db.Model):
+    """Items needed for party packages."""
+
+    __tablename__ = 'ppilists'
+
+    ppi_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    dept = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+    purchase_id = db.Column(db.Integer, db.ForeignKey('partypackages.purchase_id'))
+    inventory_id = db.Column(db.Integer, db.ForeignKey('inventorys.inventory_id'))
+
+    party_package = db.relationship('Party_Package', backref='ppilists')
+    inventory = db.Column(db.Integer, db.ForeignKey'Inventory', backrf='inventorys')
+    
+    def __repr__(self):
+        return f'<Party_Package_List pp_list_id={self.pp_list_id} name={self.name} dept={self.dept} qty={self.qty} party_cost{self.party_cost}>'
+
+class Inventory(db.Model):
+    """In house Inventory."""
+
+    __tablename__ = 'inventorys'
+
+    inventory_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    item_dept = db.Column(db.String)
+    vendor_id_num = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+    image = db.Column(db.String)
+    description = db.Column(db.String)
+    people_per_item = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return f'<Inventory inventory_id={self.inventory_id} name={self.name} 
+                    item_dept={self.item_dept} people_per_item={self.people_per_item}>'
 
 class Staffer(db.Model):
     """Staff Members."""
 
-    __tablename__ = 'staff'
+    __tablename__ = 'staffers'
 
     staff_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     fname = db.Column(db.String(50))
@@ -47,67 +123,17 @@ class Staffer(db.Model):
     def __repr__(self):
         return f'<Staffer staff_id={self.staff_id} dept={self.dept} fname={self.fname} lname={self.lname}>'
 
+class PartyStaffer(db.Model):
+    """Party Staff Members."""
 
-class Party_Package(db.Model):
-    """Party packages."""
+    __tablename__ = 'partystaffers'
 
-    __tablename__ = 'partypackages'
-
-    purchase_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String)
-    overview = db.Column(db.Text)
-    cost = db.Column(db.Float)
-    total_cost = db.Column(db.Float)
-    party_date = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('clients.user_id'))
-    staff_id = db.Column(db.Integer, db.ForeignKey('staff.staff_id'))
-
-    user = db.relationship('Client', backref='partypackages')
-    staff = db.relationship('Staffer', backref='partypackages')
-
-    def __repr__(self):
-        return f'<Party_Package purchase_id={self.purchase_id} title={self.title} total_cost{self.total_cost}>'
-
-
-class Party_Purchase_List(db.Model):
-    """Items needed for party packages."""
-
-    __tablename__ = 'pplists'
-
-    pp_list_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    dept = db.Column(db.Integer)
-    name = db.Column(db.String(50))
-    description = db.Column(db.String)
-    qty = db.Column(db.Integer)
-    available = db.Column(db.String)
-    qty_needed = db.Column(db.Integer)
-    party_cost = db.Column(db.Float)
-    purchase_id = db.Column(db.Integer, db.ForeignKey('partypackages.purchase_id'))
-    inventory_id = db.Column(db.Integer, db.ForeignKey('inventorys.inventory_id'))
-    purchase = db.relationship('Party_Package', backref='pplists')
-    inventory = db.relationship('Inventory', backref='pplists')
+    partystaff_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.purchase_id'))
+    staffer_id = db.Column(db.Integer, db.ForignKey('staffers.staff_id'))
     
     def __repr__(self):
-        return f'<Party_Package_List pp_list_id={self.pp_list_id} name={self.name} dept={self.dept} qty={self.qty} party_cost{self.party_cost}>'
-
-
-class Inventory(db.Model):
-    """In house Inventory."""
-
-    __tablename__ = 'inventorys'
-
-    inventory_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    vendor_id_num = db.Column(db.Integer)
-    name = db.Column(db.String(50))
-    image = db.Column(db.String)
-    description = db.Column(db.String)
-    qty = db.Column(db.Integer)
-    qty_sold = db.Column(db.Integer)
-    item_dept = db.Column(db.String)
-    location_warehouse = db.Column(db.String)
-    
-    def __repr__(self):
-        return f'<Inventory inventory_id={self.inventory_id} name={self.inventory} item_dept={self.item_dept} qty={self.qty}>'
+        return f'<PartyStaffer partystaff_id={self.partystaff_id}>'
 
 
 def connect_to_db(flask_app, db_uri='postgresql:///mikole', echo=True):
@@ -129,3 +155,15 @@ if __name__ == '__main__':
     # query it executes.
 
     connect_to_db(app)
+
+# Future description class on Inventory Data(db.Model)
+#     inventory_data = db.Column(db.String)
+#     qty = db.Column(db.Integer)
+#     available = db.Column(db.String)
+#     qty_needed = db.Column(db.Integer)
+#     item_cost = db.Column(db.Float)
+#     purchase_id = db.Column(db.Integer, db.ForeignKey('partypackages.purchase_id'))
+#     inventory_id = db.Column(db.Integer, db.ForeignKey('inventorys.inventory_id'))
+#     purchase = db.relationship('Party_Package', backref='ppilists')
+#     inventory = db.relationship('Inventory', backref='ppilists')
+    
